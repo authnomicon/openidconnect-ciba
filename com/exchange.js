10999bbca649e0d912b96818bcd41bdacc705226
Jared Hanson <jaredhanson@gmail.com>
2024-08-15 16:52:55 -0700

    Loading transaction in exchange.

--- com/exchange.js
+++ com/exchange.js
@@ -1,7 +1,7 @@
-var ciba = require('oauth2orize-ciba').exchange.ciba;
+var ciba = require('oauth2orize-ciba');
 
 
-exports = module.exports = function() {
+exports = module.exports = function(store) {
   
   
   // TODO: load extensions (ie, oidc)
@@ -14,12 +14,26 @@ exports = module.exports = function() {
       //  console.log('CIBA?');
       //}
       
-      return ciba(function(client, authReqID, cb) {
+      return ciba.exchange.ciba(function(client, authReqID, cb) {
         console.log('ISSUE...');
         console.log(client);
         console.log(authReqID);
         
-        return cb(null, 'G5kXH2wHvUra0sHlDy1iTkDJgsgUO1bN')
+        store.load(authReqID, function(err, txn) {
+          console.log('LOADED');
+          console.log(err);
+          console.log(txn);
+          
+          if (txn.allow == undefined) {
+            console.log('ERROR WITH PENDING....');
+            
+            return cb(new ciba.TokenError('Pending', 'authorization_pending'))
+          }
+          
+          
+        });
+        
+        //return cb(null, 'G5kXH2wHvUra0sHlDy1iTkDJgsgUO1bN')
         
       });
     });
@@ -27,3 +41,6 @@ exports = module.exports = function() {
 
 exports['@implements'] = 'module:oauth2orize.tokenRequestHandler';
 exports['@type'] = 'urn:openid:params:grant-type:ciba';
+exports['@require'] = [
+  'module:oauth2orize-ciba.TransactionStore'
+];
